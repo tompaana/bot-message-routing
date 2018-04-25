94a67f581c4aae810878f1b08acba43c950b3515
Jessica Tibaldi <jetiba@microsoft.com>
2018-04-25 11:00:56 -0700

    Create classes Connection and ConnectionRequest

---
+++ BotMessageRouting/Models/Connection.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Bot.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BotMessageRouting.Models
+{
+    [Serializable]
+    public class Connection
+    {
+        /// <summary>
+        /// Represents the last time in which user and agent interact in the connection.
+        /// TODO: We had to change the value every time there is an interaction between the two.
+        /// </summary>
+        public DateTime LastInteractionTime
+        {
+            get;
+            set;
+        }
+
+        public ConversationReference Requestor
+        {
+            get;
+            set;
+        }
+
+        public ConversationReference Approver
+        {
+            get;
+            set;
+        }
+
+        public Connection(ConversationReference requestor, ConversationReference approver)
+        {
+            Requestor = requestor;
+            Approver = approver;
+            LastInteractionTime = DateTime.Now;
+        }
+    }
+}
